754ea08e80cb1e805c64a66feff6febb5f3e83e5
590610661 weerasak chiangsoi <jame25193@gmail.com>
2018-01-22 22:00:41 +0700

    test1 again

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,208 +1,160 @@
 
-import com.sun.org.apache.xml.internal.utils.StringBufferPool;
-
-import java.util.NoSuchElementException;
 import java.util.Stack;
-import java.util.*;
+public class Homework1 {
 
-import javax.swing.JFrame;
-import javax.swing.JTree;
-import javax.swing.SwingUtilities;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.TreeNode;
-import javax.swing.JTree;
+	public static  String number;
+	public static void main(String[] args) {
 
 
+		number  = args[0];
+		char [] letter = number.toCharArray();
 
-	public class Homework1 {
+		Node root = Stack(letter);
+		String get = infix(root);
+		System.out.println(get+"="+calculate(root));
+
+	}
+	public static Node Stack(char letter []) {
+
+		Stack<Node> data = new Stack();
+		Node node,num1,num2;
+
+		for(int i=0;i<letter.length;i++) {
+
+			if(!is_operater(letter[i])) {
+
+				node = new Node(letter[i]);
+				data.push(node);
+
+			}else {
+
+				node = new Node(letter[i]);
+				num2 = data.pop();
+				num1 = data.pop();
+				node.left = num1;
+				node.right = num2;
+
+				data.push(node);
+			}
+
+		}
+
+		node = data.peek();
+		return node;
 
 
-		Node node ;
-		public static Node Stack(char letter []) {
 
-			Stack<Node> data = new Stack();
-			Node node,num1,num2;
+	}
+	public static String infix(Node node) {
 
-			for(int i=0;i<letter.length;i++) {
+		StringBuilder infix = new StringBuilder();
+		inorder(node,infix);
 
-				if(!is_operater(letter[i])) {
+		infix.deleteCharAt(0);
+		infix.deleteCharAt(infix.length()-1);
+		for(int i =1;i<infix.length()-1;i++) {
 
-					node = new Node(letter[i]);
-					data.push(node);
+			if(is_number(infix.charAt(i))) {
+				infix.deleteCharAt(i-1);
 
-				}else {
-
-					node = new Node(letter[i]);
-					num2 = data.pop();
-					num1 = data.pop();
-					node.left = num1;
-					node.right = num2;
-
-					data.push(node);
-
-
-
-
+				if(i+1<= infix.length()){
+					infix.deleteCharAt(i);
 				}
 
 			}
 
-			node = data.peek();
-			return node;
-
-
 
 		}
-		public static String infix(Node node) {
-
-			StringBuilder infix = new StringBuilder();
-			inorder(node,infix);
-
-			infix.deleteCharAt(0);
-			infix.deleteCharAt(infix.length()-1);
-			for(int i =1;i<infix.length()-1;i++) {
-
-				if(is_number(infix.charAt(i))) {
-					infix.deleteCharAt(i-1);
-
-					if(i+1<= infix.length()){
-						infix.deleteCharAt(i);
-					}
-
-				}
-
-
-			}
+//			System.out.println(infix.toString());
 
 
 
 
-			return infix.toString();
-		}
+		return infix.toString();
+	}
 
-		public static void inorder(Node node,StringBuilder infix) {
+	public static void inorder(Node node,StringBuilder infix) {
 
 
-			if (node != null) {
-				infix.append("(");
+		if (node != null) {
+			infix.append('(');
+			if (node.letter == '+') {
 				inorder(node.left, infix);
-				infix.append(node.value);
+				infix.append('+');
 				inorder(node.right, infix);
-				infix.append(")");
-			}
+			} else if (node.letter == '-') {
+				inorder(node.left, infix);
+				infix.append('-');
+				inorder(node.right, infix);
+			} else if (node.letter == '*') {
+				inorder(node.left, infix);
+				infix.append('*');
+				inorder(node.right, infix);
+			} else if (node.letter == '/') {
+				inorder(node.left, infix);
+				infix.append('/');
+				inorder(node.right, infix);
+			} else infix.append(node.letter);
+			infix.append(')');
 
-
-		}
-
-
-		public static boolean is_operater(char letter) {
-
-			if(letter== '+' || letter=='-' || letter=='*' || letter=='/') {
-
-				return true;
-			}
-			return false;
-
-		}
-
-		public static boolean is_number(char letter) {
-
-			if(letter== '1' || letter=='2' || letter=='3' || letter=='4'|| letter=='5' || letter=='6' || letter=='7'|| letter=='8' || letter=='9' || letter=='0') {
-
-				return true;
-			}
-			return false;
-
-		}
-
-		public static boolean ishasoperater(String text) {
-
-			for(int i=0;i<text.length();i++) {
-
-				if(is_operater(text.charAt(i))){
-					return true;
-				}
-
-			}
-
-			return false;
-		}
-
-		public static void calculate(Node node) {
-
-			String text = infix(node);
-			System.out.print(text + "=");
-			String text1,text2,text3;
-
-			int ans = 0 ;
-
-			while(ishasoperater(text)) {
-
-				for(int i=0;i<text.length();i++) {
-
-					if(is_operater(text.charAt(i)) && text.charAt(i-2) == '(' && text.charAt(i+2) == ')' && !is_operater(text.charAt(i-1)) && !is_operater(text.charAt(i+1)) && text.length()>=5 && i>=2 && i<= text.length()-3) {
-
-						if(text.charAt(i) == '+') ans = Integer.parseInt((String.valueOf(text.charAt(i-1))))+Integer.parseInt((String.valueOf(text.charAt(i+1))));
-						if(text.charAt(i) == '-') ans = Integer.parseInt((String.valueOf(text.charAt(i-1))))-Integer.parseInt((String.valueOf(text.charAt(i+1))));
-						if(text.charAt(i) == '*') ans = Integer.parseInt((String.valueOf(text.charAt(i-1))))*Integer.parseInt((String.valueOf(text.charAt(i+1))));
-						if(text.charAt(i) == '/') ans = Integer.parseInt((String.valueOf(text.charAt(i-1))))/Integer.parseInt((String.valueOf(text.charAt(i+1))));
-
-
-
-						text1 = text.substring(0, i-2);
-						//System.out.print(text1);
-						text2 = Integer.toString(ans);
-						//System.out.print(text2);
-						text3 = text.substring(i+3,text.length());
-						//System.out.println(text3);
-
-						text = text1 + text2 + text3;
-
-					}
-
-				}
-
-				if(text.length() == 3) {
-					if(text.charAt(1) == '+') ans = Integer.parseInt((String.valueOf(text.charAt(0))))+Integer.parseInt((String.valueOf(text.charAt(2))));
-					if(text.charAt(1) == '-') ans = Integer.parseInt((String.valueOf(text.charAt(0))))-Integer.parseInt((String.valueOf(text.charAt(2))));
-					if(text.charAt(1) == '*') ans = Integer.parseInt((String.valueOf(text.charAt(0))))*Integer.parseInt((String.valueOf(text.charAt(2))));
-					if(text.charAt(1) == '/') ans = Integer.parseInt((String.valueOf(text.charAt(0))))/Integer.parseInt((String.valueOf(text.charAt(2))));
-
-					text = Integer.toString(ans);
-					System.out.println(text);
-
-
-				}
-
-
-			}
-
-
-
-		}
-
-
-
-
-		public static void main(String[] args) {
-			// Begin of arguments input sample
-
-			if (args.length > 0) {
-				String input = args[0];
-				if (input.equalsIgnoreCase("251-*32*+")) {
-					System.out.println("(2*(5-1))+(3*2)=14");
-				}
-			}
-			// End of arguments input sample
-
-			// TODO: Implement your project here
-
-			String number  = "251-*32*+";
-			char [] letter = number.toCharArray();
-
-			Node root = Stack(letter);
-
-			calculate(root);
 
 		}
 	}
+
+	public static boolean is_operater(char letter) {
+
+		if(letter== '+' || letter=='-' || letter=='*' || letter=='/') {
+
+			return true;
+		}
+		return false;
+
+	}
+
+	public static boolean is_number(char letter) {
+
+		if(letter== '1' || letter=='2' || letter=='3' || letter=='4'|| letter=='5' || letter=='6' || letter=='7'|| letter=='8' || letter=='9' || letter=='0') {
+
+			return true;
+		}
+		return false;
+
+	}
+
+	public static boolean ishasoperater(String text) {
+
+		for(int i=0;i<text.length();i++) {
+
+			if(is_operater(text.charAt(i))){
+				return true;
+			}
+
+		}
+
+		return false;
+	}
+
+
+	public static int calculate(Node node) {
+
+		if(node != null){
+			if(node.letter == '+') {
+				return calculate(node.left) + calculate(node.right);
+			}else if(node.letter == '-') {
+				return calculate(node.left) - calculate(node.right);
+			}else if(node.letter == '*') {
+				return calculate(node.left) * calculate(node.right);
+			}else if(node.letter == '/') {
+				return calculate(node.left)  / calculate(node.right);
+			}else return Integer.parseInt((String.valueOf(node.letter)));
+
+		}return 0;
+
+
+	}
+
+
+
+
+
+}
